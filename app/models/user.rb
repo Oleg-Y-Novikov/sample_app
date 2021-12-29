b5becf739c0c15aefa8b01be0fdd926faced3f1d
Oleg-Y-Novikov <novikov.oleg.gsx@mail.ru>
2021-12-29 23:58:39 +0300

    Add index to users email

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[a-z\d_+.\-]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
 
+  before_save { email.downcase! }
+
   # аргумент presence: true — это хэш опций с одним элементом
   # фигурные скобки необязательны, если хэш передаётся последним аргументом в метод
   validates :name, presence: true, length: { maximum: 50 }
