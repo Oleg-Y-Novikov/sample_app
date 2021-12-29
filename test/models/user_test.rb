eff962869a4d3d5353a7f9f5237775915261ee0b
Oleg-Y-Novikov <novikov.oleg.gsx@mail.ru>
2021-12-30 00:34:42 +0300

    Finish a basic User model (including secure passwords)

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,7 +4,8 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "Example User", email: "example@user.ru")
+    @user = User.new(name: "Example User", email: "example@user.ru",
+                     password: "foobar", password_confirmation: "foobar")
   end
 
   # метод assert, будет успешным при возвращении true от @user.valid?, и провальным при возвращении false.
@@ -64,5 +65,15 @@ class UserTest < ActiveSupport::TestCase
     @user.email = mixed_case_email
     @user.save
     assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
   end
 end
