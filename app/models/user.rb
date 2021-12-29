8111c23eb033c1ea2231b8d59162e491645b8db5
Oleg-Y-Novikov <novikov.oleg.gsx@mail.ru>
2021-12-29 23:13:53 +0300

    Add validations User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[a-z\d_+.\-]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
+
+  # аргумент presence: true — это хэш опций с одним элементом
+  # фигурные скобки необязательны, если хэш передаётся последним аргументом в метод
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 225 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
 end
