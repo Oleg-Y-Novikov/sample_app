e62a3a1d779f556bdcba7d24470422150b5068c4
Oleg-Y-Novikov <novikov.oleg.gsx@mail.ru>
2021-12-30 19:49:10 +0300

    User exit and close session

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,6 +4,12 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  # Осуществляет выход текущего пользователя.
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
