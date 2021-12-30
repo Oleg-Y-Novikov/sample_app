e29d7917815adcdeb6eda3059213bb022a1b53dd
Oleg-Y-Novikov <novikov.oleg.gsx@mail.ru>
2021-12-30 17:49:54 +0300

    Add helper current_user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,8 @@ class SessionsController < ApplicationController
     user = User.find_by(params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       # Осуществить вход пользователя и перенаправление на страницу профиля.
+      log_in user # хелпер log_in
+      redirect_to user # Rails автоматически конвертирует его в маршрут user_url(user)
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
