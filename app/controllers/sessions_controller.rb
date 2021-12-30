3c45205ccda7a308fd9dcdb26b9d9c6c760055b4
Oleg-Y-Novikov <novikov.oleg.gsx@mail.ru>
2021-12-30 17:24:03 +0300

    Rendering with a ﬂash message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # Осуществить вход пользователя и перенаправление на страницу профиля.
     else
-      flash[:danger] = 'Invalid email/password combination' # Не совсем верно!
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
