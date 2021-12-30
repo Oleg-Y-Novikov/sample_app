5d9e010b7e88936bd35174589f4a9d5b6fc0f0de
Oleg-Y-Novikov <novikov.oleg.gsx@mail.ru>
2021-12-30 19:14:45 +0300

    Testing template changes and logging in after registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,10 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
+  # Возвращает дайджест данной строки
+  def self.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
