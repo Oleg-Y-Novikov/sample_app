fe9a85f495a8ab52eb4dc1f6ebed83859ef4a7bf
Oleg-Y-Novikov <novikov.oleg.gsx@mail.ru>
2021-12-31 00:24:06 +0300

    Add friendly call forwarding

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -46,4 +46,15 @@ module SessionsHelper
   def current_user?(user)
     user == current_user
   end
+
+  # Перенаправляет к сохраненному расположению (или по умолчанию).
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Сохраняет запрошенный URL.
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
 end
