82b0d6a58a9eaed53ce15b0b4b82c426fd41a74f
Oleg-Y-Novikov <novikov.oleg.gsx@mail.ru>
2022-01-01 14:38:32 +0300

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -85,10 +85,12 @@ class User < ApplicationRecord
     reset_sent_at < 2.hours.ago
   end
 
-  # Определяет прото-ленту.
-  # Полная реализация в "Следовании за пользователями".
+  # Возвращает ленту сообщений пользователя.
   def feed
-    Micropost.where("user_id = ?", id)
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE  follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
   end
 
   # Начать читать сообщения пользователя.
