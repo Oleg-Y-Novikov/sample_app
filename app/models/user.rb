f0da4ebdd57c344138795ab42c575595f3e6f170
Oleg-Y-Novikov <novikov.oleg.gsx@mail.ru>
2022-01-01 12:54:45 +0300

    Relationship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,15 @@
 
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+
   attr_accessor :remember_token, :activation_token, :reset_token
 
   before_save   :downcase_email
@@ -80,6 +89,21 @@ class User < ApplicationRecord
   # Полная реализация в "Следовании за пользователями".
   def feed
     Micropost.where("user_id = ?", id)
+  end
+
+  # Начать читать сообщения пользователя.
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  # Перестать читать сообщения пользователя.
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # Возвращает true, если текущий пользователь читает сообщения другого пользователя.
+  def following?(other_user)
+    following.include?(other_user)
   end
 
   private
